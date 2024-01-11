a6d5cb6bf744366cc2732b2f91a1bff315977077
Shrimpage <jphaapaniemi@gmail.com>
2024-01-11 09:47:57 +0200

    toisen tunnin loppu

--- Examples/Example_3.cpp
+++ Examples/Example_3.cpp
@@ -14,5 +14,14 @@ int main() {
     cout << &a << endl; // print out memory address of variable a(0x5ffe8c)
 
     int* ptr_a =0;
+    ptr_a = &a; //
     cout << ptr_a << endl;
+
+    int array[200] = {0,10,20,30,340,50,5};
+    array[180] = 80;
+
+    for (int i=0;i<200;i++) 
+    {
+        cout << "Value: " << array[i] << " at address: " << &array[i] << endl;
+    }
 }
